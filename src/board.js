4f79cb976af91bf40eb1153f1d47ea2d6fc73aea
magda <magdapolak96@gmail.com>
2019-02-06 23:08:32 +0100

    Snake moving on html canvas now

--- src/board.js
+++ src/board.js
@@ -1,15 +1,17 @@
 const DIRECTIONS = {
     RIGHT: 1,
     LEFT: 2,
-    UP: 3,
-    DOWN: 4
+    UP: 4,
+    DOWN: 3
 };
 
-function Board(size) {
+function Board(size, context, segmentSize) {
     this.size = size;
     this.board = [];
     this.direction = DIRECTIONS.RIGHT;
     this.snake = [];
+    this.context = context;
+    this.segmentSize = segmentSize;
 
 
 
@@ -34,7 +36,9 @@ function Board(size) {
     this.initializeSnakePosition = function(snakeSize) {
         for (let i = 0; i < snakeSize; i++) {
             this.board[this.size - 1][i] = 1;
-            this.snake.push(new SnakeSegment(i, this.size - 1));
+            let newSegment = new SnakeSegment(i, this.size - 1)
+            this.snake.push(newSegment);
+            newSegment.draw(this.context, "Green", this.segmentSize)
         }
 
     };
@@ -75,8 +79,10 @@ function Board(size) {
     };
 
     this.moveSnake = function() {
+        console.log("movesnake", this.direction)
         let tail = this.popSnakeTail();
         this.board[tail.y][tail.x] = 0;
+        tail.clear(this.context, this.segmentSize);
         let head = this.getSnakeHead();
         let newHead;
 
@@ -88,22 +94,44 @@ function Board(size) {
             case DIRECTIONS.RIGHT:
                 newHead = new SnakeSegment(head.x + 1, head.y);
                 break;
+
             case DIRECTIONS.UP:
                 newHead = new SnakeSegment(head.x, head.y - 1);
                 break;
+
             case DIRECTIONS.DOWN:
                 newHead = new SnakeSegment(head.x, head.y + 1);
                 break;
         }
-        this.snake.push(newHead)
+        this.snake.push(newHead);
         this.board[newHead.y][newHead.x] = 1;
+        newHead.draw(this.context, "Green", this.segmentSize)
+    };
+
+
+    function SnakeSegment(x, y) {
+        this.x = x;
+        this.y = y;
+
+        this.draw = function(context, color, segmentSize) {
+            let canvas_snake_segment = document.createElement("canvas");
+            canvas_snake_segment.width = segmentSize;
+            canvas_snake_segment.height = segmentSize;
+
+            let context_snake_segment = canvas_snake_segment.getContext("2d");
+            context_snake_segment.fillStyle = color;
+            context_snake_segment.strokeStyle = color;
+            context_snake_segment.fillRect(0, 0, segmentSize, segmentSize);
+
+            context.drawImage(canvas_snake_segment, this.x * segmentSize, this.y * segmentSize);
+        };
+        this.clear = function(context, segmentSize) {
+            context.clearRect(this.x * segmentSize, this.y * segmentSize,  segmentSize, segmentSize);
+        }
     }
 }
 
-function SnakeSegment(x, y) {
-    this.x = x;
-    this.y = y;
-}
+
 
 
 export default Board;
--- src/snake_canvas.js
+++ src/snake_canvas.js
@@ -5,44 +5,59 @@ import Board from "./board"
 (function gameModule () {
     let canvas;
     let context;
+    let speed =  5;
 
     canvas = document.getElementById("mainGameCanvas");
     context = canvas.getContext("2d");
-    let obstaclesFactory = new ObstaclesFactory();
-    let obstacles = obstaclesFactory.getObstacles();
-    context.drawImage(obstacles, 0, 0);
+    // let obstaclesFactory = new ObstaclesFactory();
+    // let obstacles = obstaclesFactory.getObstacles();
+    // context.drawImage(obstacles, 0, 0);
 
-
-    let snake = new Snake(50, 1000, 500);
-    context.drawImage(snake.getSnake(), 0 , 0);
-
-    let board = new Board(10);
+    //
+    // let snake = new Snake(50, 1000, 500);
+    // context.drawImage(snake.getSnake(), 0 , 0);
+    //
+    let board = new Board(10, context, 50);
     board.init(3);
-    board.displayBoard();
 
-    board.moveSnake();
-    board.displayBoard();
+    //
+    // board.moveSnake();
+    // board.displayBoard();
+    //
+    // board.moveSnake();
+    // board.displayBoard();
+    //
+    // board.changeSnakeDirection(3);
+    // board.moveSnake();
+    // board.displayBoard();
+    //
+    // board.moveSnake();
+    // board.displayBoard();
+    //
+    // board.moveSnake();
+    // board.displayBoard();
+    //
+    // board.changeSnakeDirection(2);
+    // board.moveSnake();
+    // board.displayBoard();
+    //
+    // board.changeSnakeDirection(1);
+    // board.moveSnake();
+    // board.displayBoard();
 
-    board.moveSnake();
-    board.displayBoard();
+    function render(direction) {
+        var direction = direction
+        var moveCount = 0
+        setInterval(() => {
+            board.moveSnake();
+            if(moveCount % 3 == 0) {
+                direction = (direction + 1) == 5 ? 1 : (direction + 1)
+                board.changeSnakeDirection(direction);
+            }
 
-    board.changeSnakeDirection(3);
-    board.moveSnake();
-    board.displayBoard();
+        }, 200 * speed)
 
-    board.moveSnake();
-    board.displayBoard();
+    }
 
-    board.moveSnake();
-    board.displayBoard();
-
-    board.changeSnakeDirection(2);
-    board.moveSnake();
-    board.displayBoard();
-
-    board.changeSnakeDirection(1);
-    board.moveSnake();
-    board.displayBoard();
-
-
+    render(1)
 })();
